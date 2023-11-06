eed7dd7c7e068716e90e74345c9894b3185bd8dc
Toan <toan6858@gmail.com>
2023-11-06 20:10:53 +0700

    Pause

--- src/components/Filters/FiltersSlice.js
+++ src/components/Filters/FiltersSlice.js
@@ -1,32 +1,55 @@
-const initState = {
-  search: "",
-  status: "All",
-  priority: [],
-};
+// const initState = {
+//   search: "",
+//   status: "All",
+//   priority: [],
+// };
 
-const filtersReducer = (state = initState, action) => {
-  switch (action.type) {
-    case "filters/searchFilterChange":
-      return {
-        ...state,
-        search: action.payload,
-      };
+// const filtersReducer = (state = initState, action) => {
+//   switch (action.type) {
+//     case "filters/searchFilterChange":
+//       return {
+//         ...state,
+//         search: action.payload,
+//       };
 
-    case "filters/statusFilterChange":
-      return {
-        ...state,
-        status: action.payload,
-      };
+//     case "filters/statusFilterChange":
+//       return {
+//         ...state,
+//         status: action.payload,
+//       };
 
-    case "filters/prioriryFilterChange":
-      return {
-        ...state,
-        priority: action.payload,
-      };
+//     case "filters/prioriryFilterChange":
+//       return {
+//         ...state,
+//         priority: action.payload,
+//       };
 
-    default:
-      return state;
-  }
-};
+//     default:
+//       return state;
+//   }
+// };
 
-export default filtersReducer;
+// export default filtersReducer;
+
+import { createSlice } from "@reduxjs/toolkit";
+
+export const filterSlice = createSlice({
+  name: "filters",
+  initialState: {
+    search: "",
+    status: "All",
+    priority: [],
+  },
+  reducers: {
+    searchFilterChange: (state, action) => {
+      //mutation || IMMER
+      state.search = action.payload;
+    }, // type: 'filters/searchFilterChange'
+    statusFilterChange: (state, action) => {
+      state.status = action.payload;
+    },
+    prioriryFilterChange: (state, action) => {
+      state.priority = action.payload;
+    },
+  },
+});
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,18 @@
-import { createStore } from "redux";
-import rootReducer from "./reducer";
-import { composeWithDevTools } from "redux-devtools-extension";
+// import { createStore } from "redux";
+// import rootReducer from "./reducer";
+// import { composeWithDevTools } from "redux-devtools-extension";
 
-const composedEnhancers = composeWithDevTools();
-const store = createStore(rootReducer, composedEnhancers);
+// const composedEnhancers = composeWithDevTools();
+// const store = createStore(rootReducer, composedEnhancers);
 
-export default store;
+// export default store;
+import { configureStore } from "@reduxjs/toolkit";
+import filtersReducer from "../components/Filters/FiltersSlice";
+import todoListReducer from "../components/TodoList/TodosSlice";
+
+const store = configureStore({
+  reducer: {
+    filter: filtersReducer,
+    todoList: todoListReducer,
+  },
+});
